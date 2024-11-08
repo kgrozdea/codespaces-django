cd6aab33919164b8c3f5d2f88db011fc0801cad6
Kaio <ksgrozdea@gmail.com>
2024-11-08 09:52:44 -0300

    Update views.py

--- hello_world/core/views.py
+++ hello_world/core/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from .forms import adicionaAviso
+from .models import Aviso
+
 
 def index(request):
     context = {
         "title": "Django example",
     }
     return render(request, "index.html", context)
+
+def create_aviso(request):
+    if request.method == 'POST':
+        form = AvisoForm(request.POST)
+        if form.is_valid():
+            aviso = form.save(commit=False)  # Create an instance without saving it to the database
+            aviso.user = request.user        # Set the user to the logged-in user
+            aviso.date_posted = timezone.now()  # Set the date to today's date
+            aviso.save()                     # Save the instance to the database
+            return redirect('some-view-name')  # Redirect to a success page
+    else:
+        form = AvisoForm()
+
+    return render(request, 'create_aviso.html', {'form': form})
